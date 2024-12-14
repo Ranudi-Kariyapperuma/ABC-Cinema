fefccffc1556217f5f16f0a7d18e75907ec47e7b
nckodikara@students.nsbm.ac.lk <nckodikara@students.nsbm.ac.lk>
2024-12-15 00:43:59 +0530

    modified

--- src/java/JavaMail2.java
+++ src/java/JavaMail2.java
@@ -10,19 +10,17 @@ import javax.mail.internet.MimeMessage;
 
     public class JavaMail2 {
 
-    public static void sendMail(String recepient) throws MessagingException{
+    public static void sendMail(String Email, String subject,String body) 
+            throws MessagingException{
         System.out.println("Preparing to send Email");
         
          String From="gailluna62@gmail.com";
-         String receiver ="gailluna62@gmail.com";
-         String Username="gailluna62@gmail.com";
-         String host="smtp.gmail.com";
          String password="pchh ccbl bufi qbmr ";
         
         Properties properties= new Properties();
         properties.put("mail.smtp.auth","true");
         properties.put("mail.smtp.starttls.enable","true");
-        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.host","smtp.gmail.com");
         properties.put("mail.smtp.port","587");
        
           Authenticator authenticator = new Authenticator(){
@@ -34,18 +32,21 @@ import javax.mail.internet.MimeMessage;
     };
         Session session = Session.getInstance(properties,authenticator);     
         System.out.println("Message sent Successfully");
+        
         try{
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(From));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
-        message.setSubject("ABC Cinema Payment Confirmation");
-        message.setText("Payment confirmed");
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(Email));
+        message.setSubject(subject);
+        message.setText(body);
+       
         Transport.send(message);
-        System.out.println("Email sent successfully");
+        System.out.println("Email sent successfully"+ Email);
         }
         catch(MessagingException ex){
-            System.err.println("error found"+ ex.getMessage());
-            throw new RuntimeException(ex);        }
+            ex.printStackTrace();
+            //System.err.println("error found"+ ex.getMessage());
+            }
         }
     
     }
