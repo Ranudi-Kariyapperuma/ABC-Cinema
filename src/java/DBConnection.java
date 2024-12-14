a0b4ef868bc8fe2dddcd0ff7a72fa4ebdadd1ce6
nckodikara@students.nsbm.ac.lk <nckodikara@students.nsbm.ac.lk>
2024-12-15 00:44:20 +0530

    modified

--- src/java/DBConnection.java
+++ src/java/DBConnection.java
@@ -2,7 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
+
+/*public class DBConnection {
     private static final String url = "jdbc:mysql://localhost:3306/abc_cinema";
     private static final String user= "root";
     private static final String password = "20242024";
@@ -11,4 +12,25 @@ public class DBConnection {
             throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }  
+}*/
+
+public class DBConnection {
+    public static void main(String[] args) {
+        try {
+            // Register the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish the connection
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/abc_cinema", "root", "20242024");
+            System.out.println("Connection successful!");
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("SQL error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
